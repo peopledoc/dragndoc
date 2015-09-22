90982ed246ab99b6981403ca373ba297918e1bdb
Xavier Cambar <xcambar@gmail.com>
2015-09-22 09:39:27 +0200

    Makes the app embeddable

--- config/environment.js
+++ config/environment.js
@@ -25,6 +25,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.embedded = true;
   }
 
   if (environment === 'test') {
@@ -40,7 +41,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.embedded = true;
   }
 
   return ENV;
