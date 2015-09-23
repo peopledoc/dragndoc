e8a4a640864373d7db7cbaa40f9a4d4db835ad31
Xavier Cambar <xcambar@gmail.com>
2015-09-23 17:51:45 +0200

    Translations

---
+++ app/locales/en/translations.js
@@ -0,0 +1,13 @@
+export default {
+  source: "Source",
+  composition: "Composition",
+  submit: 'Validate',
+  help_button_text: '?',
+  help: {
+    title: 'How to split a document',
+    subtitle: 'Keyboard shortcuts',
+    paragraph_1_html: 'To select non-consecutive files or folders, press and hold down the <code>Ctrl</code> key, and then <code>click</code> each item that you want to select.',
+    paragraph_2_html: 'To select a consecutive group of pages, press and hold down the <code>Shift</code> key, <code>click</code> the first item and then <code>click</code> the last item.'
+  },
+  ok: 'OK'
+};
